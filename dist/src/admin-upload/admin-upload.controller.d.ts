b9b2ef7fa115825d3437dfab0e51e8a98651de72
Pedro Roger <pedronisashi@gmail.com>
2025-12-19 14:22:54 -0300

    chore: exclude dist folder and env files from version control

--- dist/src/admin-upload/admin-upload.controller.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class AdminUploadController {
-    private getUploadPath;
-    uploadBanner(file: Express.Multer.File, req: any): Promise<{
-        url: string;
-        filename: string;
-    }>;
-}
--- dist/src/share/share.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Response } from 'express';
-import { ShareService } from './share.service';
-export declare class ShareController {
-    private readonly shareService;
-    constructor(shareService: ShareService);
-    getCourseShare(courseId: string, res: Response): Promise<void>;
-    getEventShare(eventIdOrSlug: string, res: Response): Promise<void>;
-    getEnrollShare(courseSlugOrId: string, res: Response): Promise<void>;
-}
